b26e61e3e6b8e2e1b8d1db95e38597e491a36acd
csm <csm009177@gmail.com>
2023-12-04 17:08:53 +0900

    add component about stateData

--- public/index.js
+++ public/index.js
@@ -2,8 +2,19 @@
 export function createElement(type, props, ...children) {
   return { type, props, children};
 }
+// head comoponent
+export function component(stateData) {
+  const menuItems = [];
+  for (let i=0; i<stateData.length; i++){
+    const item = stateData[i];
+    const menuItem = createElement('li', {style:"list-style: none;"}, createElement('a', {href:item.hash}, item.text));
+    menuItems.push(menuItem);
+  }
+  return createElement('div', { 
+    style:"display: flex; text-align: center; flex-direction: column; align-items: center;"}, 
+    ...menuItems);
+}
 
-//--------------------------------------------------------------------
 // 랜더링 하는것
 export function render(virtualDom) {
   if(typeof virtualDom === 'string') {
